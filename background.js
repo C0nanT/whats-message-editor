0abcb898f8d5fb20d02f067ede60b2a50137e024
Conan <conantorreswf@gmail.com>
2025-05-17 23:19:43 -0300

    style: format

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
 chrome.commands.onCommand.addListener((command) => {
-    console.log(`Comando recebido: ${command}`);
-    if (command === "edit_message") {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        console.log(`Tab ativa: ${tabs[0].id}`);
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          files: ['content.js']
-        });
-      });
-    }
-  });
-  
+	console.log(`Comando recebido: ${command}`);
+	if (command === "edit_message") {
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			console.log(`Tab ativa: ${tabs[0].id}`);
+			chrome.scripting.executeScript({
+				target: { tabId: tabs[0].id },
+				files: ["content.js"],
+			});
+		});
+	}
+});
